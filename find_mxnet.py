6bf3a8faf290aa2c08e0d63b234a7bf85e64df95
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2017-03-14 17:54:58 +0800

    Add .gitignore

--- find_mxnet.py
+++ find_mxnet.py
@@ -3,5 +3,5 @@ try:
 except ImportError:
 	import os, sys
 	curr_path = os.path.abspath(os.path.dirname(__file__))
-	sys.path.append(os.path.join("home/mxnet/python"))
+	sys.path.append(os.path.join("/home/shhs/usr/detection/mxnet_msic/mxnet/python"))
 	import mxnet as mx
